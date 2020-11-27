dd8a0723376175322e843ecf8bd28d021dc67f09
Sami <sowusuaboagye@gmail.com>
2020-11-27 11:38:01 +0000

    sign up and login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './modules/authentication/login/login.component';
+import { SignupComponent } from './modules/authentication/signup/signup.component';
+import { HomeComponent } from './modules/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { SignupComponent } from './authentication/signup/signup.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, LoginComponent, SignupComponent],
   imports: [
     CommonModule,
     SharedModule
   ],
   exports:[
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    SignupComponent
   ]
 })
 export class ModulesModule { }
